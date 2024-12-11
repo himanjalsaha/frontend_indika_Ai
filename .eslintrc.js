802a533d58aba3cf94ca1ac4738922f1136323c8
Himanjal saha <himanjalsaha97@gmail.com>
2024-12-11 17:45:41 +0530

    first

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['next', 'next/core-web-vitals'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      'jsx-a11y/role-has-required-aria-props': 'off'
+    }
+  };
+  
